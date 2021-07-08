ae33c7b798805e3c9a00fa4c2a410a0741feae9e
Gustavo <gusangulomele89@gmail.com>
2021-07-07 21:56:25 -0500

    07072021 cambios Folio PDF

--- newFolioPDF.php
+++ newFolioPDF.php
@@ -37,34 +37,34 @@
 		$muestradia = date('d');
 		$muestrames = $arrayMeses[date('m')-1];
         $muestraano = date('Y');
-        
+
     //construccion del responsable
     $get_responsable = new Consultainfo();
     $dat_responsable = $get_responsable->get_informacion("select responsable.idcatrespongrupo as cveresponsable,
-        sector.idcatsector as cvesector, 
-        sector.sectornombre as sector,responsable.noempleado, grados.nombre as grado, elemento.nombre 
+        sector.idcatsector as cvesector,
+        sector.sectornombre as sector,responsable.noempleado, grados.nombre as grado, elemento.nombre
         from catrespongrupo as responsable
         inner join personal as elemento on responsable.noempleado = elemento.noempleado
-        inner join catgrados as grados on elemento.idcatgrados = grados.idcatgrados
+        inner join catniveles as grados on elemento.idcatgrados = grados.idcatgrados
         inner join catsector as sector on elemento.idcatsector = sector.idcatsector
         where responsable.noempleado = '$responsable'");
     foreach($dat_responsable as $detresponsable);
 
     //construccion del instructor
     $get_instructor = new Consultainfo();
-    $dat_instructor = $get_instructor->get_informacion("select instructor.idcatinstructores as cveinstructor, instructor.noempleado, grados.nombre as grado, elemento.nombre 
+    $dat_instructor = $get_instructor->get_informacion("select instructor.idcatinstructores as cveinstructor, instructor.noempleado, grados.nombre as grado, elemento.nombre
         from catinstructores as instructor
         inner join personal as elemento on instructor.noempleado = elemento.noempleado
-        inner join catgrados as grados on elemento.idcatgrados = grados.idcatgrados
+        inner join catniveles as grados on elemento.idcatgrados = grados.idcatgrados
         where instructor.idcatinstructores = '$instructor'");
     foreach($dat_instructor as $detinstructor);
 
     //construccion el Jefe de Campo
     $get_revisor = new Consultainfo();
-    $dat_revisor = $get_revisor->get_informacion("select revisor.idcatrevisores as cverevisor, revisor.noempleado, grados.nombre as grado, elemento.nombre 
+    $dat_revisor = $get_revisor->get_informacion("select revisor.idcatrevisores as cverevisor, revisor.noempleado, grados.nombre as grado, elemento.nombre
         from catrevisores as revisor
         left join personal as elemento on revisor.noempleado = elemento.noempleado
-        left join catgrados as grados on elemento.idcatgrados = grados.idcatgrados
+        left join catniveles as grados on elemento.idcatgrados = grados.idcatgrados
         where revisor.idcatrevisores = '$revisor'");
     foreach($dat_revisor as $detrevisor);
 
@@ -90,9 +90,9 @@
         }
 
         $folioOBJ = new Consultainfo();
-        $ultFolio = $folioOBJ->get_informacion("select * from catfolios order by idcatfolios desc limit 1");
+        $ultFolio = $folioOBJ->get_informacion("select count(0) as total from catfolios");
         foreach($ultFolio as $detfolio);
-        $idfolio = $detfolio["idcatfolios"] + 1;
+        $idfolio = $detfolio["total"] + 1;
         $folio = generaCeros($idfolio);
 
         //Estrutura de la fecha
@@ -103,7 +103,7 @@
         $folioNuevo = $new_folio->set_informacion("insert into catfolios values(DEFAULT,'$folio','FUERZA DE TAREA','$fechacompleta','$detresponsable[cvesector]',
         '$detarma[idcatarmas]','$detresponsable[cveresponsable]','$detrevisor[cverevisor]','$detinstructor[cveinstructor]','$consumido','$casentregados',
 		'$casextraviados','$siluetas','$asistentes','$observaciones','$tpractica')");
-		
+
 		//Bitacora de Acceso
 		$accion = new Consultainfo();
 		$sql = "insert into catfolios values(DEFAULT,$folio,FUERZA DE TAREA,$fechacompleta,$detresponsable[cvesector],
@@ -114,7 +114,8 @@
 
         //////////////////////////////////////////////INicio del Reporte PDF
 		$pdf=new FPDF('P','mm','Letter');
-		$pdf->Open();
+		//$pdf->Open();
+		//$pdf->AddFont('Times','','Times.php');
 		$pdf->AddPage();
 		$pdf->SetMargins(20,20,20);
 		//Logo Ciudad de México
@@ -126,7 +127,7 @@
 		$pdf->Text(77,29,utf8_decode('Dir. Ejec. de Logística y Seguimiento Operativo'));
 		$pdf->Text(77,33,utf8_decode('Dirección de Armamento'));
 		//Cuadro de Fecha
-		$pdf->SetFont('Arial','B',11);
+		$pdf->SetFont('Times','B',11);
 		$pdf->Text(140,29,utf8_decode('FECHA'));
 		$pdf->SetLineWidth(0.4);
 		$pdf->Line(156,19,206,19);
@@ -136,7 +137,7 @@
 		$pdf->Line(167,19,167,29);
 		$pdf->Line(190,19,190,29);
 		$pdf->Line(206,19,206,29);
-		$pdf->SetFont('Arial','',11);
+		$pdf->SetFont('Times','',11);
 		$pdf->Text(158,23,utf8_decode('DÍA'));
 		$pdf->SetXY(156,24);
 		$pdf->Cell(11,5,utf8_decode($muestradia),1,1,'C');
@@ -146,28 +147,28 @@
 		$pdf->Text(193,23,utf8_decode('AÑO'));
 		$pdf->SetXY(190,24);
 		$pdf->Cell(16,5,utf8_decode($muestraano),1,1,'C');
-		$pdf->SetFont('Arial','',8);
+		$pdf->SetFont('Times','',8);
 		$pdf->Text(193,18,utf8_decode('DAR-04'));
 		//Numero de Folio del Reporte
-		$pdf->SetFont('Arial','B',11);
+		$pdf->SetFont('Times','B',11);
 		$pdf->Text(139,34,utf8_decode('FOLIO No. FT20 - '.$folio));
 		//Encabezado Negro
 		$pdf->SetLineWidth(0.7);
 		$pdf->Line(15,36,210,36);
-		$pdf->SetFont('Arial','B',16);
+		$pdf->SetFont('Times','B',16);
 		$pdf->Text(60,41,utf8_decode('RECIBO DE CONSUMO DE CARTUCHOS'));
 		$pdf->SetLineWidth(0.7);
 		$pdf->Line(15,42,210,42);
-		$pdf->SetFont('Arial','',7);
+		$pdf->SetFont('Times','',7);
 		$pdf->Text(15,46,utf8_decode('ESTE RECIBO JUSTIFICA EL CONSUMO DE CARTUCHOS POR ÁREA, CUALQUIER ALTERACIÓN SERÁ SANCIONADA CONFORME A LAS LEYES CORRESPONDIENTES'));
 		$pdf->SetLineWidth(0.2);
 		$pdf->Line(15,48,210,48);
 		$pdf->SetLineWidth(0.2);
 		$pdf->Line(15,53,210,53);
-		$pdf->SetFont('Arial','B',8.5);
+		$pdf->SetFont('Times','B',8.5);
 		$pdf->Text(29,51.5,utf8_decode('TIPO DE PRÁCTICA'));
 		$pdf->Text(127,51.5,utf8_decode('CAMPO DE TIRO'));
-		$pdf->SetFont('Arial','B',8);
+		$pdf->SetFont('Times','B',8);
 		//$pdf->Text(16,57,utf8_decode('PROGRAMA ANUAL'));
 		//$pdf->Text(46,57,utf8_decode('EXTRAORDINARIA'));
 		if($tpractica == 1){
@@ -187,16 +188,16 @@
 		$pdf->Line(45,53,45,59);
 		$pdf->Line(73,48,73,59);
 		//Campo de Tiro
-		$pdf->SetFont('Arial','B',16);
+		$pdf->SetFont('Times','B',16);
 		$pdf->Text(116,58,utf8_decode('FUERZA DE TAREA'));
-		$pdf->SetFont('Arial','B',8);
+		$pdf->SetFont('Times','B',8);
 		$pdf->Text(16,62.5,utf8_decode('ÁREA'));
 		$pdf->Text(178,62.5,utf8_decode('No. DE ASISTENTES'));
 		$pdf->Line(15,70,210,70);
 		$pdf->Line(176,59,176,70);
 		$pdf->Line(15,81,210,81);
 		$pdf->Line(55,70,55,92);
-		$pdf->SetFont('Arial','B',8);
+		$pdf->SetFont('Times','B',8);
 		$pdf->Text(21,73,utf8_decode('No DE EMPLEADO'));
 		$pdf->Text(107,73,utf8_decode('RESPONSABLE DEL GRUPO'));
 		$pdf->Line(15,92,210,92);
@@ -221,7 +222,7 @@
 		$pdf->Text(173,106,utf8_decode('CAPACITO'));
 		$pdf->Text(167,130,utf8_decode('INSTRUCTOR DE TIRO'));
 		//Espacio del Firmante del Jefe de Campo
-		$pdf->SetFont('Arial','',9);
+		$pdf->SetFont('Times','',9);
 		$pdf->SetXY(15,107);
 		$pdf->Cell(65,5,utf8_decode($detrevisor['grado']." ".$detrevisor['noempleado']),0,1,'C');
 		$pdf->SetXY(15,123);
@@ -236,17 +237,17 @@
 		$pdf->Cell(60,5,utf8_decode($detinstructor['grado']." ".$detinstructor['noempleado']),0,1,'C');
 		$pdf->SetXY(150,123);
 		$pdf->Cell(60,5,utf8_decode($detinstructor['nombre']),0,1,'C');
-		$pdf->SetFont('Arial','',11);
+		$pdf->SetFont('Times','',11);
 		$pdf->SetXY(176,63);
 		$pdf->Cell(34,7,utf8_decode($asistentes),0,1,'C');
 		//Muestra tipo de arma
-		$pdf->SetFont('Arial','',9);
+		$pdf->SetFont('Times','',9);
 		$pdf->SetXY(15,85);
 		$pdf->Cell(40,7,utf8_decode($detarma['tipoarma']),0,1,'C');
 		$pdf->SetXY(55,85);
 		$pdf->Cell(26,7,utf8_decode($detarma['calibre']),0,1,'C');
 		//Cartuchos Consumidos
-		$pdf->SetFont('Arial','',11);
+		$pdf->SetFont('Times','',11);
 		$pdf->SetXY(81,86);
 		$pdf->Cell(44,5,$consumido,0,1,'C');
 		//Casquillos entregados
@@ -259,27 +260,27 @@
 		$pdf->SetXY(15,97);
 		$pdf->Cell(41,5,$siluetas,0,1,'C');
 		//Casquillos observaciones
-		$pdf->SetFont('Arial','',7.4);
+		$pdf->SetFont('Times','',7.4);
 		$pdf->SetXY(55,97);
 		if($observaciones == ""){
 			$pdf->Cell(155,5,utf8_decode("SIN NOVEDAD"),0,1,'C');
 		}else{
 			$pdf->Cell(155,5,utf8_decode($observaciones),0,1,'C');
 		}
-		
-		
+
+
 		//Area
-		$pdf->SetFont('Arial','',11);
+		$pdf->SetFont('Times','',11);
 		$pdf->Text(75,66,utf8_decode($detresponsable['sector']));
 		//No Empleado del folio
-		$pdf->SetFont('Arial','',11);
+		$pdf->SetFont('Times','',11);
 		$pdf->Text(23,78,utf8_decode($detresponsable['noempleado']));
 		//Nombre del reponsable para el folio
-		$pdf->SetFont('Arial','',11);
+		$pdf->SetFont('Times','',11);
 		$pdf->SetXY(55,74);
 		$pdf->Cell(155,7,utf8_decode($detresponsable['nombre']),0,1,'C');
 		$pdf->Output('reporte','I');
 
 
-    
+
 ?>
